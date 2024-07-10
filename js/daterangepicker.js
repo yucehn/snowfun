f3383422bb0574d328bcdd38735cb6bedd272b83
Yu-chen Chen <s9973938@gmail.com>
2024-07-11 01:26:39 +0800

    feat: add daterangepicker

---
+++ js/daterangepicker.js
@@ -0,0 +1,39 @@
+$(function() {
+    $('#selectDaterange').daterangepicker({
+      "locale": {
+          "format": "YYYY-MM-DD",
+          "separator": " ~ ",
+          "applyLabel": "確認",
+          "cancelLabel": "取消",
+          "customRangeLabel": "自訂義範圍",
+          "daysOfWeek": [
+              "日",
+              "一",
+              "二",
+              "三",
+              "四",
+              "五",
+              "日"
+          ],
+          "monthNames": [
+              "1月",
+              "2月",
+              "3月",
+              "4月",
+              "5月",
+              "6月",
+              "7月",
+              "8月",
+              "9月",
+              "10月",
+              "11月",
+              "12月"
+          ],
+          "firstDay": 1
+      },
+      "startDate": "2024-07-05",
+      "endDate": "2024-07-11"
+    }, function(start, end, label) {
+      console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+    });
+  });
