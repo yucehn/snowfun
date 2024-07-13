a4a04a336a7bf2a56e654455bdaf055dfb317d90
Yu-chen Chen <s9973938@gmail.com>
2024-07-13 23:56:49 +0800

    fix slide

--- js/swiper.js
+++ js/swiper.js
@@ -1,5 +1,4 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
+var swiper = new Swiper(".skiResort-swiper", {
     spaceBetween: 10,
     centeredSlides: true,
     navigation: {
@@ -7,41 +6,55 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
     breakpoints: {
+      390: {
+        slidesPerView: 1.8,
+        spaceBetween: 10,
+      },
       640: {
-        slidesPerView: 1,
-        spaceBetween: 20,
+        slidesPerView: 1.8,
+        spaceBetween: 10,
       },
       768: {
-        slidesPerView: 2,
+        slidesPerView: 2.5,
         spaceBetween: 20,
       },
       1024: {
         slidesPerView: 3.5,
         spaceBetween: 20,
       },
+      1440: {
+        slidesPerView: 4.5,
+        spaceBetween: 20,
+      }
     },
   });
 
   var swiper = new Swiper(".strategy-swiper", {
-    slidesPerView: 3,
-    spaceBetween: 5,
-    loop: true,
+    slidesPerView: "auto",
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
     breakpoints: {
+      390: {
+        slidesPerView: 1.5,
+        spaceBetween: 10,
+      },
       640: {
-        slidesPerView: 1,
+        slidesPerView: 2.5,
         spaceBetween: 10,
       },
       768: {
-        slidesPerView: 2,
+        slidesPerView: 2.5,
         spaceBetween: 10,
       },
       1024: {
         slidesPerView: 3.5,
         spaceBetween: 10,
       },
+      1440: {
+        slidesPerView: 5.5,
+        spaceBetween: 10,
+      }
     },
   });
