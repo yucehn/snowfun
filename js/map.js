45fd4284c295b182826766728b94baaf358fb70c
Yu-chen Chen <s9973938@gmail.com>
2024-07-17 01:03:55 +0800

    feat: add map location

--- js/map.js
+++ js/map.js
@@ -1,111 +1,80 @@
   // When the window has finished loading create our google map below
+  var map;
+  var marker;
+
   google.maps.event.addDomListener(window, "load", init);
 
+  function updateMapPosition(latitude, longitude) {
+    var newPosition = new google.maps.LatLng(latitude, longitude);
+    var newCenter= new google.maps.LatLng(latitude, longitude + 0.3);
+    map.setCenter(newCenter);
+    marker.setPosition(newPosition);
+  }
+
   function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
-      // How zoomed in you want the map to start at (always required)
       zoom: 11,
-
-      // The latitude and longitude to center the map (always required)
-      center: new google.maps.LatLng(40.67, -73.94), // New York
-
-      // How you would like to style the map.
-      // This is where you would paste any style found on Snazzy Maps.
+      center: new google.maps.LatLng(43.17411408806869, 142.80809972631752 + 0.3),
       styles: [
         {
           featureType: "administrative",
           elementType: "labels.text.fill",
-          stylers: [
-            {
-              color: "#444444",
-            },
-          ],
+          stylers: [{ color: "#444444" }],
         },
         {
           featureType: "landscape",
           elementType: "all",
-          stylers: [
-            {
-              color: "#f2f2f2",
-            },
-          ],
+          stylers: [{ color: "#f2f2f2" }],
         },
         {
           featureType: "poi",
           elementType: "all",
-          stylers: [
-            {
-              visibility: "off",
-            },
-          ],
+          stylers: [{ visibility: "off" }],
         },
         {
           featureType: "road",
           elementType: "all",
           stylers: [
-            {
-              saturation: -100,
-            },
-            {
-              lightness: 45,
-            },
+            { saturation: -100 },
+            { lightness: 45 },
           ],
         },
         {
           featureType: "road.highway",
           elementType: "all",
-          stylers: [
-            {
-              visibility: "simplified",
-            },
-          ],
+          stylers: [{ visibility: "simplified" }],
         },
         {
           featureType: "road.arterial",
           elementType: "labels.icon",
-          stylers: [
-            {
-              visibility: "off",
-            },
-          ],
+          stylers: [{ visibility: "off" }],
         },
         {
           featureType: "transit",
           elementType: "all",
-          stylers: [
-            {
-              visibility: "off",
-            },
-          ],
+          stylers: [{ visibility: "off" }],
         },
         {
           featureType: "water",
           elementType: "all",
           stylers: [
-            {
-              color: "#ebf1f6",
-            },
-            {
-              visibility: "on",
-            },
+            { color: "#ebf1f6" },
+            { visibility: "on" },
           ],
         },
       ],
     };
 
-    // Get the HTML DOM element that will contain your map
-    // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById("map");
+    map = new google.maps.Map(mapElement, mapOptions);
 
-    // Create the Google Map using our element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-
-    // Let's also add a marker while we're at it
-    var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(40.67, -73.94),
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(43.17411408806869, 142.80809972631752),
+      // animation: google.maps.Animation.BOUNCE,
+      // icon: 'https://cdn2.iconfinder.com/data/icons/travel-solid-world-is-beautiful/512/Plane-24.png',
+      // title: 'sahoro',
+      // label: 'have fun',
       map: map,
-      title: "Snazzy!",
     });
+    
   }
