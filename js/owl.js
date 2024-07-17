cf67271047bd5ed7fa8adfc56c372e0817773ccc
Yu-chen Chen <s9973938@gmail.com>
2024-07-18 00:47:53 +0800

    fix: swiper => owl

--- js/owl.js
+++ js/owl.js
@@ -30,12 +30,15 @@ jQuery(document).ready(function ($) {
   let slider = $(".owl-carousel-strategy");
   slider.each(function () {
     $(".owl-carousel-strategy").owlCarousel({
+      startPosition: 0,
       nav: true,
       loop: false,
       dots: false,
       pagination: false,
-      margin: 20,
+      margin: 10,
       autoHeight: false,
+      // center: true,
+      navClass:['strategy-prev','strategy-next'],
       responsive: {
         0: {
           items: 1.3,
@@ -59,8 +62,8 @@ jQuery(document).ready(function ($) {
           stagePadding: 20,
         },
         1440: {
-          items: 3.1,
-          stagePadding: 20,
+          items: 3.2,
+          stagePadding: 10,
         }
       },
     });
@@ -79,3 +82,51 @@ jQuery(document).ready(function ($) {
     );
   });
 });
+
+$('.owl-carousel-skiResort').owlCarousel({
+  nav: true,
+  loop: false,
+  dots: false,
+  pagination: false,
+  margin: 20,
+  autoHeight: false,
+  center: true,
+  navClass:['skiResort-prev','skiResort-next'],
+  responsive:{
+      0:{
+        items: 1.2,
+        center: false,
+      },
+      390: {
+        items: 1.4,
+        center: false,
+      },
+      576:{
+        items: 1.6,
+        center: false,
+      },
+      768:{
+        items: 2.6
+      },
+      992:{
+        items: 2.8
+      },
+      1120: {
+        items: 3.4
+      }
+  }
+});
+
+$('.owl-carousel-skiResort.owl-nav').css('opacity', '0');
+$('.owl-carousel-skiResort').hover(
+  function() {
+    // 滑鼠進入時顯示按鈕
+    $('.owl-carousel-skiResort .owl-nav').css('opacity', '1');
+    // .css('opacity', '1');
+  },
+  function() {
+    // 滑鼠離開時隱藏按鈕
+    $('.owl-carousel-skiResort .owl-nav').css('opacity', '0');
+  }
+);
+
