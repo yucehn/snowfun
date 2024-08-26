7c41d4e2f61ad3255e67a6042e85b205eb7b714a
lee <clee@citiansolutions.com>
2024-08-26 16:52:04 +0800

    rwd ing

--- js/owl.js
+++ js/owl.js
@@ -1,160 +1,158 @@
-$('.owl-carousel-coach').owlCarousel({
-  loop:true,
-  margin: 12,
-  nav:false,
-  dots:false,
-  center: true,
-  autoplay: true,
-  slideTransition: 'linear',
-  autoplayTimeout: 2000,
-  autoplaySpeed: 2000,
-  autoplayHoverPause: true,
-  responsive:{
-      0:{
-        items:1.5,
-        center: false,
-        stagePadding: 10,
-      },
-      576:{
-        items:2.5,
-        center: false,
-        stagePadding: 10,
-      },
-      992:{
-        items:3.5
-      },
-      1200:{
-        items:4.5
-      },
-      1400:{
-        items:5.5
-    }
-  }
+$(".owl-carousel-coach").owlCarousel({
+	loop: true,
+	margin: 12,
+	nav: false,
+	dots: false,
+	center: true,
+	autoplay: true,
+	slideTransition: "linear",
+	autoplayTimeout: 2000,
+	autoplaySpeed: 2000,
+	autoplayHoverPause: true,
+	responsive: {
+		0: {
+			items: 1.5,
+			center: false,
+			stagePadding: 10,
+		},
+		576: {
+			items: 2.5,
+			center: false,
+			stagePadding: 10,
+		},
+		992: {
+			items: 3.5,
+		},
+		1200: {
+			items: 4.5,
+		},
+		1400: {
+			items: 5.5,
+		},
+	},
 });
-
 
 $(".owl-carousel-strategy").owlCarousel({
-  nav: true,
-  loop: false,
-  dots: false,
-  pagination: false,
-  margin: 10,
-  autoHeight: false,
-  center: true,
-  navClass:['strategy-prev','strategy-next'],
-  responsive: {
-    0: {
-      startPosition: 0,
-      items: 1.3,
-      stagePadding: 0,
-      margin: 10,
-      center: false,
-      loop: true,
-      nav:false,
-    },
-    470: {
-      startPosition: 0,
-      items: 1.3,
-      stagePadding: 10,
-      center: false,
-      loop: true,
-      nav:false,
-    },
-    640: {
-      startPosition: 0,
-      items: 1.5,
-      stagePadding: 20,
-      center: false,
-      loop: true,
-      nav:false,
-    },
-    768: {
-      startPosition: 0,
-      items: 1.8,
-      stagePadding: 20,
-      center: false,
-      loop: true,
-      nav:false,
-    },
-    992: {
-      startPosition: 1,
-      items: 2.7,
-      stagePadding: 20,
-    },
-    1440: {
-      startPosition: 1,
-      items: 3.5,
-      stagePadding: 10,
-    }
-  },
+	nav: true,
+	loop: false,
+	dots: false,
+	pagination: false,
+	margin: 10,
+	autoHeight: false,
+	center: true,
+	navClass: ["strategy-prev", "strategy-next"],
+	responsive: {
+		0: {
+			startPosition: 0,
+			items: 1.3,
+			stagePadding: 0,
+			margin: 10,
+			center: false,
+			loop: true,
+			nav: false,
+		},
+		470: {
+			startPosition: 0,
+			items: 1.3,
+			stagePadding: 10,
+			center: false,
+			loop: true,
+			nav: false,
+		},
+		640: {
+			startPosition: 0,
+			items: 1.5,
+			stagePadding: 20,
+			center: false,
+			loop: true,
+			nav: false,
+		},
+		768: {
+			startPosition: 0,
+			items: 1.8,
+			stagePadding: 20,
+			center: false,
+			loop: true,
+			nav: false,
+		},
+		992: {
+			startPosition: 1,
+			items: 2.7,
+			stagePadding: 20,
+		},
+		1440: {
+			startPosition: 1,
+			items: 3.5,
+			stagePadding: 10,
+		},
+	},
 });
 
-$('.strategy .owl-nav').css('opacity', '0');
-$('.owl-carousel-strategy').hover(
-  function() {
-    // 滑鼠進入時顯示按鈕
-    $('.strategy .owl-nav').css('opacity', '1');
-    // .css('opacity', '1');
-  },
-  function() {
-    // 滑鼠離開時隱藏按鈕
-    $('.strategy .owl-nav').css('opacity', '0');
-  }
+$(".strategy .owl-nav").css("opacity", "0");
+$(".owl-carousel-strategy").hover(
+	function() {
+		// 滑鼠進入時顯示按鈕
+		$(".strategy .owl-nav").css("opacity", "1");
+		// .css('opacity', '1');
+	},
+	function() {
+		// 滑鼠離開時隱藏按鈕
+		$(".strategy .owl-nav").css("opacity", "0");
+	}
 );
 
-$('.owl-carousel-skiResort').owlCarousel({
-  nav: true,
-  loop: false,
-  dots: false,
-  pagination: false,
-  margin: 20,
-  autoHeight: false,
-  center: true,
-  navClass:['skiResort-prev','skiResort-next'],
-  responsive:{
-      0:{
-        items: 1.2,
-        center: false,
-        nav:false,
-        loop: true,
-      },
-      390: {
-        items: 1.4,
-        center: false,
-        nav:false,
-        loop: true,
-      },
-      576:{
-        items: 1.6,
-        center: false,
-        loop: true,
-        nav:false,
-      },
-      768:{
-        items: 2.6,
-        center: false,
-        loop: true,
-        nav:false,
-      },
-      992:{
-        items: 2.8
-      },
-      1120: {
-        items: 3.4
-      }
-  }
+$(".owl-carousel-skiResort").owlCarousel({
+	nav: true,
+	loop: false,
+	dots: false,
+	pagination: false,
+	margin: 20,
+	autoHeight: false,
+	center: true,
+	navClass: ["skiResort-prev", "skiResort-next"],
+	responsive: {
+		0: {
+			items: 1.2,
+			center: false,
+			nav: false,
+			loop: true,
+		},
+		390: {
+			items: 1.4,
+			center: false,
+			nav: false,
+			loop: true,
+		},
+		576: {
+			items: 1.6,
+			center: false,
+			loop: true,
+			nav: false,
+		},
+		768: {
+			items: 2.6,
+			center: false,
+			loop: true,
+			nav: false,
+		},
+		992: {
+			items: 2.8,
+		},
+		1120: {
+			items: 3.4,
+		},
+	},
 });
 
-$('.owl-carousel-skiResort.owl-nav').css('opacity', '0');
-$('.owl-carousel-skiResort').hover(
-  function() {
-    // 滑鼠進入時顯示按鈕
-    $('.owl-carousel-skiResort .owl-nav').css('opacity', '1');
-    // .css('opacity', '1');
-  },
-  function() {
-    // 滑鼠離開時隱藏按鈕
-    $('.owl-carousel-skiResort .owl-nav').css('opacity', '0');
-  }
+$(".owl-carousel-skiResort.owl-nav").css("opacity", "0");
+$(".owl-carousel-skiResort").hover(
+	function() {
+		// 滑鼠進入時顯示按鈕
+		$(".owl-carousel-skiResort .owl-nav").css("opacity", "1");
+		// .css('opacity', '1');
+	},
+	function() {
+		// 滑鼠離開時隱藏按鈕
+		$(".owl-carousel-skiResort .owl-nav").css("opacity", "0");
+	}
 );
-
